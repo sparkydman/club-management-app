a604a0aa665ca6ed947983f866498d83a9911139
Chigozie Ugwuede <sparkyglobe@gmail.com>
2021-03-07 16:43:02 +0100

    updated

--- models/index.js
+++ models/index.js
@@ -10,7 +10,14 @@ const db = {};
 
 let sequelize;
 if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
   sequelize = new Sequelize(
     config.database,
