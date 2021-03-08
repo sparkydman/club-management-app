3a11c6ae4549600b65ce240623663da55bc515d4
Chigozie Ugwuede <sparkyglobe@gmail.com>
2021-03-08 04:58:10 +0100

    updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+const favicon = require('express-favicon');
 
 const models = require('./models');
 const errorHandler = require('./middleware/error-handler.js');
